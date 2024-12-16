00dedb0432455d2312ec159ea97e17f8245d2719
Rob van Bakel <robbakel@gmail.com>
2024-12-16 18:40:39 +0000

    Add cache

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 import { Hono } from "hono";
+import type { KVNamespace } from "@cloudflare/workers-types";
 
 import { fetchData } from "./utils/fetchData";
 import { generateIcs } from "./utils/generateIcs";
 import { parsePeriod } from "./utils/parsePeriod";
 
-const app = new Hono();
+const cacheKey = "efteling-onderhoudskalender-cache";
 
+const app = new Hono<{
+  Bindings: {
+    [cacheKey]: KVNamespace;
+  };
+}>();
 app.get("/feed", async (c) => {
+  const feedCache = await c.env[cacheKey].get("feed");
+
+  if (feedCache) {
+    return c.body(feedCache, 200, {
+      "Content-Type": "text/calendar",
+    });
+  }
+
   const attractions = await fetchData();
 
   const parsedData = attractions.map((attraction) => ({
@@ -15,6 +29,10 @@ app.get("/feed", async (c) => {
   }));
 
   const icsString = generateIcs(parsedData);
+
+  await c.env[cacheKey].put("feed", icsString, {
+    expirationTtl: 3600,
+  });
 
   return c.body(icsString, 200, {
     "Content-Type": "text/calendar",
